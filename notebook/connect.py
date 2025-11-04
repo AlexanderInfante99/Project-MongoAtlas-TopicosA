2b3b74debd7a0e21a8a9857028e0cce3a9ca20e3
AlexanderInfante99 <alexinfantechacon@gmail.com>
2025-11-04 12:46:45 -0500

    Configurando la conexión a MongoBD Atlas and view collections

---
+++ notebook/connect.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient, errors
+from dotenv import load_dotenv
+import os
+
+#Cargar variables de entorno
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGODB_URI_ATLAS')
+DB_NAME_ATLAS = os.getenv('MONGODB_DATA')
+
+try:
+    client = MongoClient(MONGO_URI)
+    db = client[DB_NAME_ATLAS]
+    print("Conexión exitosa a Atlas")
+    colecciones = db.list_collection_names()
+    print('Conectado MongoDB Atlas: Base de datos', (DB_NAME_ATLAS))
+    print('Colecciones', (colecciones))
+except errors.ServerSelectionTimeoutError as e:
+    print("No se pudo conectar al servidor", e)
+except errors.OperationFailure as e:
+    print('Error de autenticación', e)
+except Exception as e:
+    print('No sé que pasa', e)
